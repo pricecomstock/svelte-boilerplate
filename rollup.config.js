f56337370a2f38a4f13c138c350bff8853230c20
Price Comstock <price.comstock@gmail.com>
2019-04-28 14:04:51 -0500

    enabled live reload

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -34,6 +36,9 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+		serve("public"),
+		livereload("public")
 	]
 };
